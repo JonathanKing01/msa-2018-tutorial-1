cd1b1333a1729fbab1d7e98473fd8158f6fa874f
JonathanKing01 <Jon_ok@hotmail.co.uk>
2018-08-27 15:24:35 +1200

    Adding material UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,21 @@
 import * as React from 'react';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
-import { IndexLinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+import {AppBar, IconButton, Toolbar, Typography} from '@material-ui/core/';
 
-export const Header: React.StatelessComponent<{}> = () => (
-    <Navbar>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <Link to="/">DankMemes</Link>
-            </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-            <IndexLinkContainer to="/FirstComponent">
-                <NavItem>Page 1</NavItem>
-            </IndexLinkContainer>
-            <IndexLinkContainer to="/SecondComponent">
-                <NavItem>Page 2</NavItem>
-            </IndexLinkContainer>
-        </Nav>
-    </Navbar>
-);
+export const Header: React.StatelessComponent<{}> = () => {
+    return (
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton  aria-label="Menu" color="inherit">
+                        <MenuIcon aria-haspopup="true"/>
+                    </IconButton>
+                    <Typography variant="display2" color="inherit">
+                        <Link style={{color: "white"}} to="/">dankNotDank</Link>
+                        <Link to="/FirstComponent"> Page 1 </Link>
+                        <Link to="/SecondComponent"> Page 2 </Link>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+    );
+}
